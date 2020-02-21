fe0ea14de2ca4d9f5ac65eb34aefdd49d7f79e7b
Sebastian Malm <skmalm@gmail.com>
2020-02-21 16:40:11 -0500

    fix: Get initial drawn cards to fly from deck

--- Animated Set/Custom Views/CardGridView.swift
+++ Animated Set/Custom Views/CardGridView.swift
@@ -10,88 +10,17 @@ import UIKit
 
 //@IBDesignable
 class CardGridView: UIView {
-    
-    // Implicit unwrapping safe because property is set in controller property's didSet
-    weak var viewController: ViewController!
-    
-    var modelCards = [ModelCard]() { didSet { setNeedsLayout() }}
-    
-    var selectedCards = Set<ModelCard>()
-    
-    // clear is used as a default value
-    var borderColor: CGColor = UIColor.clear.cgColor
+    weak var superviewController: ViewController!
     
     override func layoutSubviews() {
         super.layoutSubviews()
         for subview in subviews {
             subview.removeFromSuperview()
         }
-        generateViewCards()
+        superviewController.generateViewCards()
         for subview in subviews {
-            let tap = UITapGestureRecognizer(target: viewController, action: #selector(viewController.tapCard(byHandlingGestureRecognizedBy:)))
+            let tap = UITapGestureRecognizer(target: superviewController, action: #selector(superviewController.tapCard(byHandlingGestureRecognizedBy:)))
             subview.addGestureRecognizer(tap)
         }
-    }
- 
-    var previousGrid: Grid?
-    
-    private func generateViewCards() {
-        var grid = Grid(layout: .aspectRatio(Constants.cardAspectRatio), frame: bounds)
-        grid.cellCount = modelCards.count
-        var gridTracker = 0
-        for modelCard in modelCards {
-            var cardFrame = CGRect(origin: bounds.origin, size: grid.cellSize)
-            if previousGrid != nil {
-                cardFrame = previousGrid?[gridTracker]?.inset(by: self.insetSize) ?? CGRect.zero
-            }
-            let card = CardView(fromModelCard: modelCard, withFrame: cardFrame)
-            card.contentMode = .redraw
-            // "face down" cards have a huge brown border covering the card
-            card.layer.borderColor = UIColor.brown.cgColor
-            if modelCard.isFaceUp {
-                card.layer.borderWidth = 0.0
-            } else {
-                card.layer.borderWidth = 100.0
-            }
-            addSubview(card)
-            UIViewPropertyAnimator.runningPropertyAnimator(
-                withDuration: Constants.flyInDuration,
-                delay: 0,
-                options: [],
-                animations: { card.frame = grid[gridTracker]?.inset(by: self.insetSize) ?? CGRect.zero },
-                completion: { finished in
-                    if !modelCard.isFaceUp {
-                        UIView.transition(
-                        with: card,
-                        duration: 0.5,
-                        options: [.transitionFlipFromLeft],
-                        animations: { card.layer.borderWidth = 0.0 })
-                    }
-            })
-            if self.selectedCards.contains(modelCard) {
-                card.layer.borderWidth = Constants.cellInsetValue
-                card.layer.borderColor = self.borderColor
-            }
-            gridTracker += 1
-        }
-        previousGrid = grid
-    }
-}
-
-extension CGRect {
-    func inset(by size: CGSize) -> CGRect {
-        return insetBy(dx: size.width, dy: size.height)
-    }
-}
-
-extension CardGridView {
-    private struct Constants {
-        static let cardAspectRatio: CGFloat = 0.57
-        static let cellInsetValue: CGFloat = 4.0
-        static let selectionBorderWidth: CGFloat = 5.0
-        static let flyInDuration = 0.5
-    }
-    private var insetSize: CGSize {
-        return CGSize(width: Constants.cellInsetValue, height: Constants.cellInsetValue)
     }
 }
--- Animated Set/ViewController.swift
+++ Animated Set/ViewController.swift
@@ -15,9 +15,66 @@ class ViewController: UIViewController {
         startNewGame()
     }
 
-    @IBOutlet weak var cardGridView: CardGridView! { didSet {
-        cardGridView.viewController = self
-        }}
+    private var game: SetGame!
+    private var cheatMode = false
+    
+    private func startNewGame() {
+        game = SetGame()
+        cheatMode = false
+        previousGrid = nil
+        updateUI()
+    }
+    
+    private var allCardViews: [CardView] {
+        return cardGridView.subviews as! [CardView]
+    }
+    
+    var modelCards = [ModelCard]() { didSet { cardGridView.setNeedsLayout() }}
+    var selectedCards = Set<ModelCard>()
+    
+    var previousGrid: Grid?
+        
+    func generateViewCards() {
+        var grid = Grid(layout: .aspectRatio(Constants.cardAspectRatio), frame: cardGridView.bounds)
+        grid.cellCount = modelCards.count
+        var gridTracker = 0
+        for modelCard in modelCards {
+            var cardFrame = CGRect(origin: CGPoint(x: 0, y: -104.5), size: deckCardView!.frame.size)
+            if previousGrid != nil {
+                cardFrame = previousGrid?[gridTracker]?.inset(by: self.insetSize) ?? CGRect.zero
+            }
+            let card = CardView(fromModelCard: modelCard, withFrame: cardFrame)
+            card.contentMode = .redraw
+            // "face down" cards have a huge brown border covering the card
+            card.layer.borderColor = UIColor.brown.cgColor
+            if modelCard.isFaceUp {
+                card.layer.borderWidth = 0.0
+            } else {
+                card.layer.borderWidth = 100.0
+            }
+            cardGridView.addSubview(card)
+            UIViewPropertyAnimator.runningPropertyAnimator(
+                withDuration: Constants.flyInDuration,
+                delay: 0,
+                options: [],
+                animations: { card.frame = grid[gridTracker]?.inset(by: self.insetSize) ?? CGRect.zero },
+                completion: { finished in
+                    if !modelCard.isFaceUp {
+                        UIView.transition(
+                            with: card,
+                            duration: 0.5,
+                            options: [.transitionFlipFromLeft],
+                            animations: { card.layer.borderWidth = 0.0 })
+                    }
+            })
+            if self.selectedCards.contains(modelCard) {
+                card.layer.borderWidth = Constants.cellInsetValue
+                card.layer.borderColor = borderColor()
+            }
+            gridTracker += 1
+        }
+        previousGrid = grid
+    }
     
     @objc func tapCard(byHandlingGestureRecognizedBy recognizer: UITapGestureRecognizer) {
         if recognizer.state == .ended {
@@ -26,28 +83,13 @@ class ViewController: UIViewController {
             // cardviews themselves have a modelCard set on initialization
             let tappedModelCard = tappedViewCard.modelCard!
             game.select(tappedModelCard)
-            updateViewFromModel()
+            updateUI()
         }
     }
-        
-    private var game: SetGame!
     
     // cards are identified by indices in the game.cards array
     
-    private var cheatMode = false
-    
-    private func startNewGame() {
-        game = SetGame()
-        cheatMode = false
-        cardGridView.previousGrid = nil
-        updateViewFromModel()
-    }
-    
-    private var allCardViews: [CardView] {
-        return cardGridView.subviews as! [CardView]
-    }
-    
-    private func updateViewFromModel() {
+    private func updateUI() {
         guard game.setAvailable() || game.cardsInDeck.count >= 3 else {
             multiplierLabel.text = "Game over!"
             return
@@ -58,8 +100,8 @@ class ViewController: UIViewController {
         if cheatMode { multiplierLabel.text = "Cheat Mode" }
         scoreLabel.text = cheatMode ? "Score: N/A" : "Score: \(game.score)"
         
-        cardGridView.selectedCards = game.selectedCards
-        cardGridView.modelCards = game.availableCards
+        selectedCards = game.selectedCards
+        modelCards = game.availableCards
 
         // manage deal button and cheat button based on game state
         if !game.setAvailable() {
@@ -75,7 +117,6 @@ class ViewController: UIViewController {
         } else { disableDealButton() }
         // if no cards left in deck, disable deal button
         if game.cardsInDeck.count < 3 { disableDealButton() }
-        cardGridView.borderColor = borderColor()
         game.flipAllAvailableCardsFaceUp()
     }
     
@@ -106,7 +147,7 @@ class ViewController: UIViewController {
     
     @IBAction private func pressDealButton(_ sender: UIButton) {
         game.dealThreeCards()
-        updateViewFromModel()
+        updateUI()
     }
     
     @IBAction private func touchNewGameButton(_ sender: UIButton) {
@@ -116,7 +157,7 @@ class ViewController: UIViewController {
     @IBAction private func touchCheatButton(_ sender: UIButton) {
         game.cheat()
         cheatMode = true
-        updateViewFromModel()
+        updateUI()
     }
     
     @IBOutlet weak var discardCardView: CardView! { didSet {
@@ -135,10 +176,27 @@ class ViewController: UIViewController {
     @IBOutlet private weak var deckCountLabel: UILabel!
     @IBOutlet private weak var dealButton: UIButton! { didSet {
         dealButton.layer.cornerRadius = Constants.dealButtonCornerRadius }}
+    @IBOutlet weak var cardGridView: CardGridView! { didSet {
+        cardGridView.superviewController = self
+        cardGridView.clipsToBounds = false
+        }}
 }
 
 extension ViewController {
     private struct Constants {
         static let dealButtonCornerRadius: CGFloat = 12.0
+        static let cardAspectRatio: CGFloat = 0.57
+        static let cellInsetValue: CGFloat = 4.0
+        static let selectionBorderWidth: CGFloat = 5.0
+        static let flyInDuration = 0.5
+    }
+    private var insetSize: CGSize {
+        return CGSize(width: Constants.cellInsetValue, height: Constants.cellInsetValue)
+    }
+}
+
+extension CGRect {
+    func inset(by size: CGSize) -> CGRect {
+        return insetBy(dx: size.width, dy: size.height)
     }
 }
