73b44d3d678ba543b4b1b6ba78bb5c5177c460ce
Sebastian Malm <skmalm@gmail.com>
2020-02-24 17:38:36 -0500

    fix: Dynamic Animation: increase magnitude, disallow rotation, increase resistance

--- Animated Set/Custom Views/DynamicCardBehavior.swift
+++ Animated Set/Custom Views/DynamicCardBehavior.swift
@@ -16,10 +16,17 @@ class DynamicCardBehavior: UIDynamicBehavior {
         return behavior
     }()
     
+    lazy var itemBehavior: UIDynamicItemBehavior = {
+        let behavior = UIDynamicItemBehavior()
+        behavior.allowsRotation = false
+        behavior.resistance = 0.8
+        return behavior
+    }()
+    
     func push(_ item: UIDynamicItem) {
         let push = UIPushBehavior(items: [item], mode: .instantaneous)
         push.angle = CGFloat.random(in: 0...2*CGFloat.pi)
-        push.magnitude = CGFloat.random(in: 1...3)
+        push.magnitude = 5.0
         push.action = { [unowned push, weak self] in
             self?.removeChildBehavior(push)
         }
@@ -28,16 +35,19 @@ class DynamicCardBehavior: UIDynamicBehavior {
     
     func add(_ item: UIDynamicItem) {
         collisionBehavior.addItem(item)
+        itemBehavior.addItem(item)
         push(item)
     }
     
     func remove(_ item: UIDynamicItem) {
         collisionBehavior.removeItem(item)
+        itemBehavior.removeItem(item)
     }
     
     override init() {
         super.init()
         addChildBehavior(collisionBehavior)
+        addChildBehavior(itemBehavior)
     }
     
     convenience init(inAnimator animator: UIDynamicAnimator) {
