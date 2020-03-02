e1f21e813d07e5d0866870aeeebfb8a30d643a93
Sebastian Malm <skmalm@gmail.com>
2020-03-02 15:01:42 -0500

    refactor: Give themes identifiers to avoid localization issues

--- Animated Set/Concentration MVC/Concentration Model/Theme.swift
+++ Animated Set/Concentration MVC/Concentration Model/Theme.swift
@@ -11,8 +11,19 @@ import Foundation
 import UIKit
 
 struct Theme {
+    static var identifierTracker = 0
+    let identifier: Int
     let name: String
     let backgroundColor: UIColor
     let cardColor: UIColor
     let emojis: [String]
+    
+    init(name: String, backgroundColor: UIColor, cardColor: UIColor, emojis: [String]) {
+        self.identifier = Theme.identifierTracker
+        Theme.identifierTracker += 1
+        self.name = name
+        self.backgroundColor = backgroundColor
+        self.cardColor = cardColor
+        self.emojis = emojis
+    }
 }
